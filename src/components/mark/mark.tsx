7559952140c36db79aaca799d87261cc8196bd68
Greg Thompson <thompson.glowe@gmail.com>
2019-07-26 12:43:03 -0500

    WIP: euimark

---
+++ src/components/mark/mark.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent, HTMLAttributes } from 'react';
+import classnames from 'classnames';
+import { CommonProps } from '../common';
+
+type EuiMarkProps = CommonProps &
+  HTMLAttributes<HTMLElement> & {
+    type?: 'mark' | 'strong' | 'em';
+  };
+
+export const EuiMark: FunctionComponent<EuiMarkProps> = ({
+  children,
+  className,
+  type = 'mark',
+}) => {
+  const classes = classnames('euiMark', [`euiMark--${type}`], className);
+  return <mark className={classes}>{children}</mark>;
+};
